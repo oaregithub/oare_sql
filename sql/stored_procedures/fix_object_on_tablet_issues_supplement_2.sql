444be4b964461822900015c7966df5d55a042faa
Gertrudius <Gertrudius@gmail.com>
2023-05-24 10:17:09 -0600

    cleaned up a few unused variables, and updated the main query to exclude signs in discourse_units that occur on multiple lines

--- sql/stored_procedures/fix_object_on_tablet_issues_supplement_2.sql
+++ sql/stored_procedures/fix_object_on_tablet_issues_supplement_2.sql
@@ -1,7 +1,7 @@
 CREATE DEFINER=`oare`@`%` PROCEDURE `fix_object_on_tablet_issues_supplement_2`()
 BEGIN
         DECLARE done INT DEFAULT FALSE;
-		DECLARE this_uuid, this_text_uuid, this_parent_uuid, this_discourse_uuid, previous_uuid, previous_text_uuid, previous_parent_uuid, previous_discourse_uuid CHAR(36) DEFAULT NULL;
+		DECLARE this_uuid, this_text_uuid, this_parent_uuid, this_discourse_uuid CHAR(36) DEFAULT NULL;
 		DECLARE this_type VARCHAR(250);
         DECLARE this_object_on_tablet, this_char_on_tablet, this_char_on_line, this_min, this_max, this_previous_min, this_previous_max, iter, eval, istrue INT DEFAULT 0;
         DECLARE this_order_num, comp DECIMAL(7,3) DEFAULT NULL;
