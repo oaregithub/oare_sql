606e82870d638511571d206e972875cabdb61cdc
Gertrudius <Gertrudius@gmail.com>
2023-05-24 11:25:27 -0600

    fixed some errors that crept in when I updated the repository to the latest triggers

--- sql/triggers/before_insert/before_dictionary_spelling_insert.sql
+++ sql/triggers/before_insert/before_dictionary_spelling_insert.sql
@@ -1,9 +1,9 @@
-DROP TRIGGER before_dictionary_word_insert;
+DROP TRIGGER before_dictionary_spelling_epigraphy_insert;
 DELIMITER //
-CREATE TRIGGER before_dictionary_word_insert
+CREATE TRIGGER before_dictionary_spelling_epigraphy_insert
 BEFORE INSERT
-ON `dictionary_word` FOR EACH ROW
+ON `dictionary_spelling_epigraphy` FOR EACH ROW
 BEGIN
-	INSERT INTO uuid (uuid, table_reference) VALUES (`new`.uuid, "dictionary_word");
+	INSERT INTO uuid (uuid, table_reference) VALUES (`new`.uuid, "dictionary_spelling_epigraphy");
 END //
 DELIMITER ;
