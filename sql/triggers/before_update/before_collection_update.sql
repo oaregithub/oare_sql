620dcce510cacb03aefb6a07694cd161a8c3a5a6
Gertrudius <Gertrudius@gmail.com>
2023-05-30 15:05:24 -0600

    added missing triggers to before_update triggers * before_asset_update * before_collection_update * before_dictionary_spelling_epigraphy_update * before_value_update

---
+++ sql/triggers/before_update/before_collection_update.sql
@@ -0,0 +1,6 @@
+CREATE TRIGGER before_collection_update
+BEFORE UPDATE
+ON `collection` FOR EACH ROW
+BEGIN
+	INSERT INTO `logging`(`type`, `time`, `reference_table`, `uuid`, `object_values`) VALUES ("UPDATE",SYSDATE(),"collection",`old`.`uuid`,concat("prim_archive¦",COALESCE(`old`.`name`,'NULL'));
+END
---
+++ sql/triggers/before_update/before_dictionary_spelling_epigraphy_update.sql
@@ -0,0 +1,6 @@
+CREATE TRIGGER before_dictionary_spelling_epigraphy_update
+BEFORE UPDATE
+ON `dictionary_spelling_epigraphy` FOR EACH ROW
+BEGIN
+	INSERT INTO `logging`(`type`, `time`, `reference_table`, `uuid`, `object_values`) VALUES ("UPDATE",SYSDATE(),"dictionary_spelling_epigraphy",`old`.`uuid`,concat("reference_uuid¦",COALESCE(`old`.`reference_uuid`,'NULL'),"¦spelling¦",COALESCE(`old`.`spelling`,'NULL'),"¦explicit_spelling¦",COALESCE(`old`.`explicit_spelling`,'NULL')));
+END
