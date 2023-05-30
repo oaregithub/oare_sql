8fdc49b9407f716368d342357d86124057718686
Gertrudius <Gertrudius@gmail.com>
2023-05-30 15:45:00 -0600

    fixed concatenation issue

--- sql/triggers/before_update/before_collection_update.sql
+++ sql/triggers/before_update/before_collection_update.sql
@@ -2,5 +2,5 @@ CREATE TRIGGER before_collection_update
 BEFORE UPDATE
 ON `collection` FOR EACH ROW
 BEGIN
-	INSERT INTO `logging`(`type`, `time`, `reference_table`, `uuid`, `object_values`) VALUES ("UPDATE",SYSDATE(),"collection",`old`.`uuid`,concat("name",COALESCE(`old`.`name`,'NULL')));
+	INSERT INTO `logging`(`type`, `time`, `reference_table`, `uuid`, `object_values`) VALUES ("UPDATE",SYSDATE(),"collection",`old`.`uuid`,concat("name¦",COALESCE(`old`.`name`,'NULL')));
 END
