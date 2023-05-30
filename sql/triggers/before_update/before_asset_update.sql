1f84688d47869ff9aded17223b191a137f8b5caf
Gertrudius <Gertrudius@gmail.com>
2023-05-30 15:38:36 -0600

    Fixed some errors in: before_asset_update before_collection_update before_dictionary_spelling_epigraphy_update

--- sql/triggers/before_update/before_asset_update.sql
+++ sql/triggers/before_update/before_asset_update.sql
@@ -2,5 +2,5 @@ CREATE TRIGGER before_asset_update
 BEFORE UPDATE
 ON `asset` FOR EACH ROW
 BEGIN
-	INSERT INTO `logging`(`type`, `time`, `reference_table`, `uuid`, `object_values`) VALUES ("UPDATE",SYSDATE(),"asset",`old`.`uuid`,concat("reference_uuid¦",COALESCE(`old`.`reference_uuid`,'NULL'),"¦sign_spell_num¦",COALESCE(`old`.`sign_spell_num`,'NULL'),"¦reading_uuid¦",COALESCE(`old`.`reading_uuid`,'NULL'),"¦reading¦",COALESCE(`old`.`reading`,'NULL'),"¦sign_uuid¦",COALESCE(`old`.`sign_uuid`,'NULL')));
+	INSERT INTO `logging`(`type`, `time`, `reference_table`, `uuid`, `object_values`) VALUES ("UPDATE",SYSDATE(),"asset",`old`.`uuid`,concat("prim_archive",COALESCE(`old`.`prim_archive`,'NULL'),"¦asset_ser_no¦",COALESCE(`old`.`asset_ser_no`,'NULL')));
 END
--- sql/triggers/before_update/before_dictionary_spelling_epigraphy_update.sql
+++ sql/triggers/before_update/before_dictionary_spelling_epigraphy_update.sql
@@ -2,5 +2,5 @@ CREATE TRIGGER before_dictionary_spelling_epigraphy_update
 BEFORE UPDATE
 ON `dictionary_spelling_epigraphy` FOR EACH ROW
 BEGIN
-	INSERT INTO `logging`(`type`, `time`, `reference_table`, `uuid`, `object_values`) VALUES ("UPDATE",SYSDATE(),"dictionary_spelling_epigraphy",`old`.`uuid`,concat("reference_uuid¦",COALESCE(`old`.`reference_uuid`,'NULL'),"¦spelling¦",COALESCE(`old`.`spelling`,'NULL'),"¦explicit_spelling¦",COALESCE(`old`.`explicit_spelling`,'NULL')));
+	INSERT INTO `logging`(`type`, `time`, `reference_table`, `uuid`, `object_values`) VALUES ("UPDATE",SYSDATE(),"dictionary_spelling_epigraphy",`old`.`uuid`,concat("reference_uuid¦",COALESCE(`old`.`reference_uuid`,'NULL'),"¦sign_spell_num¦",COALESCE(`old`.`sign_spell_num`,'NULL'),"¦reading_uuid¦",COALESCE(`old`.`reading_uuid`,'NULL'),"¦reading¦",COALESCE(`old`.`reading`,'NULL'),"¦sign_uuid¦",COALESCE(`old`.`sign_uuid`,'NULL')));
 END
