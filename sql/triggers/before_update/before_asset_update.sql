2016893f23f8f7dccac5e76abfc0b921dac9a000
Gertrudius <Gertrudius@gmail.com>
2023-05-30 15:43:32 -0600

    added a mising ¦ in the concatenation in: before_collection_update

--- sql/triggers/before_update/before_asset_update.sql
+++ sql/triggers/before_update/before_asset_update.sql
@@ -2,5 +2,5 @@ CREATE TRIGGER before_asset_update
 BEFORE UPDATE
 ON `asset` FOR EACH ROW
 BEGIN
-	INSERT INTO `logging`(`type`, `time`, `reference_table`, `uuid`, `object_values`) VALUES ("UPDATE",SYSDATE(),"asset",`old`.`uuid`,concat("prim_archive",COALESCE(`old`.`prim_archive`,'NULL'),"¦asset_ser_no¦",COALESCE(`old`.`asset_ser_no`,'NULL')));
+	INSERT INTO `logging`(`type`, `time`, `reference_table`, `uuid`, `object_values`) VALUES ("UPDATE",SYSDATE(),"asset",`old`.`uuid`,concat("prim_archive¦",COALESCE(`old`.`prim_archive`,'NULL'),"¦asset_ser_no¦",COALESCE(`old`.`asset_ser_no`,'NULL')));
 END
