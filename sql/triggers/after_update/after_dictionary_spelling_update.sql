811da372e2060a545b583a482146e36a7f22bc80
Gertrudius <Gertrudius@gmail.com>
2023-05-31 12:02:39 -0600

    Added logic to after_dictionary_spelling_update to limit it to running when a change to explicit_spelling occurs in the update.

--- sql/triggers/after_update/after_dictionary_spelling_update.sql
+++ sql/triggers/after_update/after_dictionary_spelling_update.sql
@@ -2,5 +2,7 @@ CREATE TRIGGER after_dictionary_spelling_update
 AFTER UPDATE
 ON `dictionary_spelling` FOR EACH ROW
 BEGIN
-	CALL add_dictionary_spelling_epigraphy(NEW.uuid);
+	IF (`old`.explicit_spelling != NEW.explicit_spelling) THEN
+		CALL add_dictionary_spelling_epigraphy(NEW.uuid);
+	END IF;
 END
