1d21e430f2a3327513a3778e0282a3d5f3d6df61
Gertrudius <Gertrudius@gmail.com>
2023-06-08 14:27:03 -0600

    Fixed some errors from the previous updates

--- sql/stored_procedures/iterate_object_on_tablet_on_delete.sql
+++ sql/stored_procedures/iterate_object_on_tablet_on_delete.sql
@@ -1,6 +1,6 @@
 CREATE PROCEDURE `iterate_object_on_tablet_on_delete`(IN this_uuid CHAR(36))
 	BEGIN
-        DECLARE tthis_object_on_tablet INT DEFAULT 0;
+        DECLARE this_object_on_tablet INT DEFAULT 0;
 		DECLARE this_text_uuid CHAR(36) DEFAULT '';
         SELECT text_uuid INTO this_text_uuid FROM text_epigraphy WHERE uuid = CONVERT(this_uuid USING latin1) COLLATE latin1_swedish_ci; 
 		SELECT object_on_tablet INTO this_object_on_tablet FROM text_epigraphy WHERE uuid = CONVERT(this_uuid USING latin1) COLLATE latin1_swedish_ci;
--- sql/stored_procedures/iterate_word_on_tablet_after_insert.sql
+++ sql/stored_procedures/iterate_word_on_tablet_after_insert.sql
@@ -4,6 +4,6 @@ CREATE PROCEDURE `iterate_word_on_tablet_after_insert`(IN this_uuid CHAR(36))
 		DECLARE this_text_uuid CHAR(36) DEFAULT '';
 		SELECT text_uuid INTO this_text_uuid FROM text_discourse WHERE uuid = CONVERT(this_uuid USING latin1) COLLATE latin1_swedish_ci;
 		SELECT word_on_tablet INTO this_word_on_tablet FROM text_discourse WHERE uuid = CONVERT(this_uuid USING latin1) COLLATE latin1_swedish_ci;
-		--might need to adjust logic to more explicity identify actual words
+		-- might need to adjust logic to more explicity identify actual words
 		UPDATE text_discourse SET word_on_tablet = (word_on_tablet+1) WHERE text_uuid = CONVERT(this_text_uuid USING latin1) COLLATE latin1_swedish_ci AND word_on_tablet IS NOT NULL AND word_on_tablet >= (this_word_on_tablet+1) AND uuid != this_uuid  ORDER BY obj_in_text ASC;
     END
