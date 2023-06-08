25233646593ee930f1bdc13bb516e239518c30a2
Gertrudius <Gertrudius@gmail.com>
2023-06-08 12:55:28 -0600

    Removed unused variables and added logic to check if the parent is a line.

--- sql/stored_procedures/iterate_char_on_line_after_insert.sql
+++ sql/stored_procedures/iterate_char_on_line_after_insert.sql
@@ -1,13 +1,10 @@
 CREATE PROCEDURE `iterate_char_on_line_after_insert`(IN this_uuid CHAR(36))
 	BEGIN
-        DECLARE this_char_on_line, this_char_on_tablet, this_object_on_tablet INT DEFAULT 0;
-		DECLARE this_text_uuid, this_parent_uuid CHAR(36) DEFAULT '';
-		
-        SELECT text_uuid INTO this_text_uuid FROM text_epigraphy WHERE uuid = CONVERT(this_uuid USING latin1) COLLATE latin1_swedish_ci; 
+		DECLARE this_char_on_line INT DEFAULT 0;
+		DECLARE this_parent_uuid CHAR(36) DEFAULT '';
 		SELECT parent_uuid INTO this_parent_uuid FROM text_epigraphy WHERE uuid = CONVERT(this_uuid USING latin1) COLLATE latin1_swedish_ci; 
-		SELECT char_on_line INTO this_char_on_line FROM text_epigraphy WHERE uuid = CONVERT(this_uuid USING latin1) COLLATE latin1_swedish_ci; 
-		SELECT char_on_tablet INTO this_char_on_tablet FROM text_epigraphy WHERE uuid = CONVERT(this_uuid USING latin1) COLLATE latin1_swedish_ci; 
-		SELECT object_on_tablet INTO this_object_on_tablet FROM text_epigraphy WHERE uuid = CONVERT(this_uuid USING latin1) COLLATE latin1_swedish_ci; 
-        
-		UPDATE text_epigraphy SET char_on_line = (char_on_line+1) WHERE parent_uuid = CONVERT(this_parent_uuid USING latin1) COLLATE latin1_swedish_ci AND char_on_line IS NOT NULL AND char_on_line >= (this_char_on_line+1) AND uuid != this_uuid  ORDER BY object_on_tablet ASC;
+		IF ((SELECT `type` FROM text_epigraphy WHERE parent_uuid = CONVERT(this_parent_uuid USING latin1) COLLATE latin1_swedish_ci) = 'line') THEN
+			SELECT char_on_line INTO this_char_on_line FROM text_epigraphy WHERE uuid = CONVERT(this_uuid USING latin1) COLLATE latin1_swedish_ci; 
+			UPDATE text_epigraphy SET char_on_line = (char_on_line+1) WHERE parent_uuid = CONVERT(this_parent_uuid USING latin1) COLLATE latin1_swedish_ci AND char_on_line IS NOT NULL AND char_on_line >= (this_char_on_line+1) AND uuid != this_uuid  ORDER BY object_on_tablet ASC;
+		END IF;
     END
