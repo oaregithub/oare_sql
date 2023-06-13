dcee0b57c86f98b1d91f23acf8819a5c9644d281
Gertrudius <Gertrudius@gmail.com>
2023-06-13 14:26:37 -0600

    This procedure was not working correctly, it is now.

--- sql/stored_procedures/text_epigraphy_de_iterate_line.sql
+++ sql/stored_procedures/text_epigraphy_de_iterate_line.sql
@@ -1,26 +1,19 @@
-CREATE PROCEDURE `text_epigraphy_de_iterate_line`(IN this_text_uuid CHAR(36), IN this_line decimal(5,2))
-	BEGIN
-		DECLARE done INT DEFAULT FALSE;
-		DECLARE that_uuid CHAR(36);
-		DECLARE that_parent_uuid CHAR(36);
-        DECLARE that_line decimal(5,2);
-		DECLARE cur1 CURSOR FOR SELECT uuid, parent_uuid, line FROM text_epigraphy WHERE parent_uuid IN (SELECT uuid FROM text_epigraphy WHERE text_uuid = this_text_uuid AND `type` = 'line');
-		DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = TRUE;
-        
-        SET @line:=this_line-2;
-		UPDATE text_epigraphy SET `line` = @line := @line+1 WHERE text_uuid = this_text_uuid AND `type` = 'line' AND `line` >= this_line;
-        
-        		OPEN cur1;
-	
-		read_loop: LOOP
-			FETCH cur1 INTO that_uuid, that_parent_uuid, that_line;
-			IF done THEN
-			LEAVE read_loop;
-			END IF;
-			
-            UPDATE text_epigraphy SET line = (SELECT line FROM text_epigraphy WHERE uuid = that_parent_uuid) WHERE uuid = that_uuid;
+CREATE PROCEDURE `text_epigraphy_de_iterate_line`(IN this_uuid CHAR(36))
+BEGIN
+		DECLARE this_text_uuid CHAR(36);
+		DECLARE this_line, this_decimal DECIMAL(5,2);
+        DECLARE this_whole INT;
+        DECLARE this_remainder VARCHAR(250);
 
-		END LOOP;
-		CLOSE cur1;
-        
-    END
+        SELECT line INTO this_line FROM text_epigraphy WHERE uuid = CONVERT(this_uuid USING latin1) COLLATE latin1_swedish_ci;
+		SELECT text_uuid INTO this_text_uuid FROM text_epigraphy WHERE uuid = CONVERT(this_uuid USING latin1) COLLATE latin1_swedish_ci;
+		SET this_whole = TRUNCATE(this_line,0);
+		SET this_decimal = this_line - this_whole;
+        SET this_remainder = REPLACE(this_decimal, "0.", ".");
+        SET this_remainder = CONCAT("\\",this_remainder);
+		
+		UPDATE text_epigraphy SET line = line-1 
+			WHERE text_uuid = CONVERT(this_text_uuid USING latin1) COLLATE latin1_swedish_ci 
+ 			AND line >= this_line
+            AND line REGEXP this_remainder;
+	END
