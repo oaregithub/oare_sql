f41561d35a70686d8f4e21e498f421fdb1e80de9
Gertrudius <81266071+Gertrudius@users.noreply.github.com>
2024-05-01 14:40:56 -0600

    added missing SELECT statement for the new_explicit_spelling variable in update_discourse_transcription

--- sql/stored_procedures/update_discourse_transcription.sql
+++ sql/stored_procedures/update_discourse_transcription.sql
@@ -8,25 +8,26 @@ BEGIN
         SELECT transcription INTO old_transcription FROM text_discourse WHERE uuid = CONVERT(this_discourse_uuid USING latin1) COLLATE latin1_swedish_ci;
         SELECT form INTO new_transcription FROM dictionary_form WHERE uuid = (SELECT reference_uuid FROM dictionary_spelling WHERE uuid = CONVERT(new_spell_uuid USING latin1) COLLATE latin1_swedish_ci);
 		SELECT spelling_uuid INTO new_spell_uuid FROM text_discourse WHERE uuid = CONVERT(this_discourse_uuid USING latin1) COLLATE latin1_swedish_ci;
+        SELECT explicit_spelling INTO new_explicit_spelling FROM text_discourse WHERE uuid = CONVERT(this_discourse_uuid USING latin1) COLLATE latin1_swedish_ci;
         SELECT `type` INTO new_type FROM text_discourse WHERE uuid = CONVERT(this_discourse_uuid USING latin1) COLLATE latin1_swedish_ci;
 		SELECT COUNT(explicit_spelling) INTO new_spelling_count FROM dictionary_spelling WHERE explicit_spelling = new_explicit_spelling GROUP BY explicit_spelling;
 		SELECT uuid INTO possible_spelling_uuid FROM dictionary_spelling WHERE explicit_spelling = new_explicit_spelling GROUP BY explicit_spelling;
         IF ((new_spell_uuid = '') OR (new_type = 'number')) THEN
-			UPDATE text_discourse SET transcription = NULL WHERE uuid = CONVERT(this_discourse_uuid USING latin1) COLLATE latin1_swedish_ci;        
+			UPDATE text_discourse SET transcription = NULL WHERE uuid = CONVERT(this_discourse_uuid USING latin1) COLLATE latin1_swedish_ci;  
 		ELSEIF (new_type = 'region') THEN
-            UPDATE text_discourse SET transcription = old_transcription WHERE uuid = CONVERT(this_discourse_uuid USING latin1) COLLATE latin1_swedish_ci; 
+            UPDATE text_discourse SET transcription = old_transcription WHERE uuid = CONVERT(this_discourse_uuid USING latin1) COLLATE latin1_swedish_ci;
         ELSE
 			IF (new_transcription != old_transcription) THEN
 			    IF (new_transcription = '') THEN
-				    UPDATE text_discourse SET transcription = NULL WHERE uuid = CONVERT(this_discourse_uuid USING latin1) COLLATE latin1_swedish_ci; 
+				    UPDATE text_discourse SET transcription = NULL WHERE uuid = CONVERT(this_discourse_uuid USING latin1) COLLATE latin1_swedish_ci;
                 ELSE
-                    UPDATE text_discourse SET transcription = new_transcription WHERE uuid = CONVERT(this_discourse_uuid USING latin1) COLLATE latin1_swedish_ci; 
+                    UPDATE text_discourse SET transcription = new_transcription WHERE uuid = CONVERT(this_discourse_uuid USING latin1) COLLATE latin1_swedish_ci;
                 END IF;
             END IF;
 		END IF;
         IF (new_spelling_count = 1) THEN
 			UPDATE text_discourse SET spelling_uuid = possible_spelling_uuid, transcription = new_transcription WHERE uuid = CONVERT(this_discourse_uuid USING latin1) COLLATE latin1_swedish_ci; 
 		ELSE 
-			UPDATE text_discourse SET spelling_uuid = NULL, transcription = NULL WHERE uuid = CONVERT(this_discourse_uuid USING latin1) COLLATE latin1_swedish_ci; 
+			UPDATE text_discourse SET spelling_uuid = NULL, transcription = NULL WHERE uuid = CONVERT(this_discourse_uuid USING latin1) COLLATE latin1_swedish_ci;
         END IF;
 END
