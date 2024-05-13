10ea528b30a6d7c4d82d106dd061b14b4c424f96
Gertrudius <81266071+Gertrudius@users.noreply.github.com>
2024-05-13 14:23:59 -0600

    added some missing support for deleting forms and words.

--- sql/triggers/before_delete/before_dictionary_form_delete.sql
+++ sql/triggers/before_delete/before_dictionary_form_delete.sql
@@ -3,6 +3,7 @@ BEFORE DELETE
 ON `dictionary_form` FOR EACH ROW
 BEGIN
 	SET FOREIGN_KEY_CHECKS=0;
+	DELETE FROM dictionary_spelling WHERE dictionary_spelling.reference_uuid = `old`.uuid;
 	DELETE FROM item_properties WHERE item_properties.reference_uuid = `old`.uuid;
 	DELETE FROM item_properties WHERE item_properties.object_uuid = `old`.uuid;
     SET FOREIGN_KEY_CHECKS=1;
--- sql/triggers/before_delete/before_dictionary_spelling_delete.sql
+++ sql/triggers/before_delete/before_dictionary_spelling_delete.sql
@@ -3,6 +3,7 @@ BEFORE DELETE
 ON `dictionary_spelling` FOR EACH ROW
 BEGIN
 	SET FOREIGN_KEY_CHECKS=0;
+	UPDATE text_discourse SET spelling_uuid = NULL, transcription = NULL WHERE text_discourse.spelling_uuid = `old`.uuid;
 	DELETE FROM item_properties WHERE item_properties.reference_uuid = `old`.uuid;
 	DELETE FROM item_properties WHERE item_properties.object_uuid = `old`.uuid;
     SET FOREIGN_KEY_CHECKS=1;
